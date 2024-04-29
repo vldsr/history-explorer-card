ed70d81472c666f8b38ba01f0d62db241a4316ce
JC <joshua@coales.co.uk>
2024-04-29 22:15:15 +0100

    Adding a very rough build script, to try and build a simulacrum of the original build artifact

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "(function () {" >> output.js
+
+# Copy in the dependencies
+cat deps/moment.js >> output.js
+cat deps/chart.js >> output.js
+cat deps/timeline.js >> output.js
+cat deps/md5.js >> output.js
+cat deps/filesaver.js >> output.js
+
+# Copy in the actual sources
+cat src/languages.js >> output.js
+grep -v "^import " src/history-explorer-card.js >> output.js  # Copy without the import lines
+cat src/history-chart-vline.js >> output.js
+cat src/history-default-colors.js >> output.js
+cat src/history-csv-exporter.js >> output.js
+cat src/history-info-panel.js >> output.js
+
+echo "})();" >> output.js
