d485bcc669358cd41cb2a1f3ba7d68b9a9c32ad1
JC <joshua@coales.co.uk>
2024-05-01 23:30:39 +0100

    Don't build a separate licence file, just include it in the artifact

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   output: {
     filename: '../history-explorer-card.js',
   },
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
 };
